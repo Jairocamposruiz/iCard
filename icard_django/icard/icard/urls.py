7aab264bab86a5557688050e3c9a150c2ca3a695
Jairo Campos Ruiz <Jairocamposruiz1991@gmail.com>
2022-06-27 11:34:23 +0200

    Initializing React project and configuring documentation in django project

--- icard_django/icard/icard/urls.py
+++ icard_django/icard/icard/urls.py
@@ -16,6 +16,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="iCard - ApiDocs",
+        default_version='v1',
+        description="iCard Api Documentation",
+        terms_of_service="https://jcoder.es",
+        contact=openapi.Contact(email="jairocamposruiz1991@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
